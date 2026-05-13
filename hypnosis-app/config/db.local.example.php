f895115ad406a2ad377fa530cf1c79057355d0ca
Dr Sergio <sgclobo@gmail.com>
2026-05-13 10:09:33 +0900

    Fixed connection issues.

--- hypnosis-app/config/db.local.example.php
+++ hypnosis-app/config/db.local.example.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 return [
     'host' => '127.0.0.1',
     'port' => 3306,
-    'name' => 'hypnosis_app',
-    'user' => 'root',
-    'pass' => '',
+    'name' => 'u781534777_hypnosis',
+    'user' => 'u781534777_hypnotherapist',
+    'pass' => 'S3rgiio@1060',
     'charset' => 'utf8mb4',
 ];
